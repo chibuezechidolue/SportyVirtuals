b4afea3adad21d0f3eaf480a893e32705a41d04e
Stanley Chidolue <chidoluechibueze@gmail.com>
2025-08-24 11:23:33 +0100

    modified the read_data functionality to save read data output in text file

--- CheckPattern/read_data.py
+++ CheckPattern/read_data.py
@@ -67,7 +67,7 @@
 #                 print(f"{league_id}: failed")
 
 
-with open("../ScrapeData/data1.txt", "r") as file:
+with open("../ScrapeData/data.txt", "r") as file:
     txt_file = file.readlines()
 
 for line in txt_file:
@@ -76,35 +76,80 @@ for line in txt_file:
         won = False
         a = {}
         for league_id, league in new_season.items():
-            a[league_id] = {1: None, 2: None, 3: None, 4: None,
-                            5: None, 6: None, 7: None, 8: None, 9: None}
+            a[league_id] = {}
             # Rotate the dictionary to start from week1 instead of week34
             league = dict(list(league.items())[::-1])
             for week, matches in league.items():
-                if week == "Week 21":
-                    break
+                a[league_id][week] = {1: None, 2: None, 3: None, 4: None,
+                                      5: None, 6: None, 7: None, 8: None, 9: None}
+                # if week == "Week 21":
+                #     break
                 count = 1
                 for teams, outcome in matches.items():
-                    # if outcome['correct_score'][0] == "3-3":
-                    # if outcome['correct_score'][0] == "4-0" or outcome['correct_score'][0] == "0-4":
-                    # if outcome['correct_score'][0] == "4-1" or outcome['correct_score'][0] == "1-4":
-                    # if outcome['ht/ft'][0] == "1/2" or outcome['correct_score'][0] == "3-3":
-                    # if outcome['ht/ft'][0] == "2/1" or outcome['correct_score'][0] == "3-3":
-                    if outcome['ht/ft'][0] == "1/2" or outcome['ht/ft'][0] == "2/1":
-                        won = True
-                        a[league_id][count] = "won"
+                    # if not "X/" in outcome['ht/ft'][0]:
+                    if outcome['ht/ft'][0][:2] == "X/":
+                        # won = True
+                        a[league_id][week][count] = "won"
                         # dic = a[league_id]
                         # a[league_id] = dict(sorted(dic.items()))
-                    # if count == 11:
+                    # if count == 3:
                     #     break
                     count += 1
                 # print(week)
-                if won:
+                if not None in a[league_id][week].values():
+                    # if list(a[league_id][week].values()).count("won") == 9:
+                    won = True
                     break
             if won:
-                print(f"{league_id}: won")
+                with open('read_data_output.txt', 'at') as file:
+                    print(f"{league_id}: won", file=file)
                 break
             else:
-                # print(f"{league_id}: failed")
-                print(a)
+                with open('read_data_output.txt', 'at') as file:
+                    print(f"{league_id}: lost", file=file)
+                # print(a)
+
                 a = {}
+
+
+# with open("../ScrapeData/data1.txt", "r") as file:
+#     txt_file = file.readlines()
+
+# for line in txt_file:
+#     if line[2:11] == "League ID":
+#         exec(f"new_season={line}")
+#         won = False
+#         a = {}
+#         for league_id, league in new_season.items():
+#             a[league_id] = {1: None, 2: None, 3: None, 4: None,
+#                             5: None, 6: None, 7: None, 8: None, 9: None}
+#             # Rotate the dictionary to start from week1 instead of week34
+#             league = dict(list(league.items())[::-1])
+#             for week, matches in league.items():
+#                 if week == "Week 21":
+#                     break
+#                 count = 1
+#                 for teams, outcome in matches.items():
+#                     # if outcome['correct_score'][0] == "3-3":
+#                     # if outcome['correct_score'][0] == "4-0" or outcome['correct_score'][0] == "0-4":
+#                     # if outcome['correct_score'][0] == "4-1" or outcome['correct_score'][0] == "1-4":
+#                     # if outcome['ht/ft'][0] == "1/2" or outcome['correct_score'][0] == "3-3":
+#                     # if outcome['ht/ft'][0] == "2/1" or outcome['correct_score'][0] == "3-3":
+#                     if outcome['ht/ft'][0] == "1/2" or outcome['ht/ft'][0] == "2/1":
+#                         won = True
+#                         a[league_id][count] = "won"
+#                         # dic = a[league_id]
+#                         # a[league_id] = dict(sorted(dic.items()))
+#                     # if count == 11:
+#                     #     break
+#                     count += 1
+#                 # print(week)
+#                 if won:
+#                     break
+#             if won:
+#                 print(f"{league_id}: won")
+#                 break
+#             else:
+#                 # print(f"{league_id}: failed")
+#                 print(a)
+#                 a = {}
